a84ec936b4c817632cc7d04b24a43257fd78a25a
Mo Bitar <me@bitar.io>
2018-09-03 11:53:15 -0500

    Assets

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -6,7 +6,7 @@ Rails.application.config.assets.version = '1.0'
 # Add additional assets to the asset load path.
 # Rails.application.config.assets.paths << Emoji.images_path
 # Add Yarn node_modules folder to the asset load path.
-Rails.application.config.assets.paths << Rails.root.join('node_modules')
+# Rails.application.config.assets.paths << Rails.root.join('node_modules')
 
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in the app/assets
@@ -14,4 +14,4 @@ Rails.application.config.assets.paths << Rails.root.join('node_modules')
 # Rails.application.config.assets.precompile += %w( admin.js admin.css )
 
 Rails.application.config.assets.paths << Rails.root.join('node_modules', 'sn-stylekit', 'dist')
-Rails.application.config.assets.precompile += %w( stylekit.css )
+Rails.application.config.assets.precompile += %w( sn-stylekit/dist/stylekit.css )
